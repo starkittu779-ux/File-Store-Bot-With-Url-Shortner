fc46284425018d18f6e4954da4de563345808c2f
starkittu779-ux <starkittu779@gmail.com>
2026-09-20 12:13:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,17 +5,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "35230011"))
-  API_HASH = os.environ.get("API_HASH", "470b35d32367cbfa09bb8f5a113f9007")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8904921120:AAFdCnwfx15DiIuo9NmXpX5Ogi2JjbOpIys")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "File_gstore_bot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1004459905559"))
+  API_ID = int(os.environ.get("API_ID", "34449133"))
+  API_HASH = os.environ.get("API_HASH", "187d76e11adae63492edefcbbc7c93f9")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8170362977:AAHOT9qQhx3iRJUI7oIGw1C0mWxoYH-FZzg")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "My_gmsbot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1004359592853"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1486683643"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://collabwithinaya_db_user:6bmMdmN7iw8iOwQZ@cluster0.k2tj2jx.mongodb.net/?appName=Cluster0")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "") ṁ
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "8354149675"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Rony-bot:Rony@582245@cluster0.c96njbd.mongodb.net/?appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1004413718506"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003867403801"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
